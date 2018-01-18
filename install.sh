399d72d4530fbc5685307b1e139829675e07b355
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-01-17 23:46:32 -0600

    updating installation scripts with testing mods

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ sudo pip3 install -r requirements.txt
 
 # create the required directories and copy the supporting files
 sudo mkdir /opt/suricata-json-consumer
-sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.py
 sudo cp suricata-json-consumer.py /opt/suricata-json-consumer/
 sudo cp suricata-json-consumer.toml /opt/suricata-json-consumer/
-
-# sudo cp suricata-json-consumer.sh /opt/suricata-json-consumer/
+sudo cp suricata-json-consumer.sh /opt/suricata-json-consumer/
+sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.py
+sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.sh
 # sudo cp svc-suricata-json-consumer.sh /opt/suricata-json-consumer/
 
 # adjust the permisions
@@ -17,12 +17,12 @@ sudo cp suricata-json-consumer.toml /opt/suricata-json-consumer/
 # sudo chmod a+x /opt/suricata-json-consumer/svc-suricata-json-consumer.sh
 
 # copy start-up files and add the Ubuntu service
-# sudo cp svc-suricata-json-consumer.sh /etc/init.d/svc-suricata-json-consumer
-# sudo chmod a+x /etc/init.d/svc-suricata-json-consumer
-# sudo update-rc.d svc-suricata-json-consumer enable
+sudo cp svc-suricata-json-consumer.sh /etc/init.d/suricata-json-consumer
+sudo chmod a+x /etc/init.d/suricata-json-consumer
+sudo update-rc.d suricata-json-consumer defaults
+sudo update-rc.d suricata-json-consumer enable
 
-
-sudo cp suricata-json-consumer.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable suricata-json-consumer.service
-sudo systemctl start suricata-json-consumer.service
+#sudo cp suricata-json-consumer.service /etc/systemd/system/
+#sudo systemctl daemon-reload
+#sudo systemctl enable suricata-json-consumer.service
+#sudo systemctl start suricata-json-consumer.service
