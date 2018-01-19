e937a335aba196d0f9a31ed79b1b44d1481b8adf
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-01-18 22:25:49 -0600

    working installation

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install the required python packages
-sudo pip3 install -r requirements.txt
+pip3 install --force-reinstall -r requirements.txt 
 
 # create the required directories and copy the supporting files
 sudo mkdir /opt/suricata-json-consumer
@@ -10,19 +10,11 @@ sudo cp suricata-json-consumer.toml /opt/suricata-json-consumer/
 sudo cp suricata-json-consumer.sh /opt/suricata-json-consumer/
 sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.py
 sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.sh
-# sudo cp svc-suricata-json-consumer.sh /opt/suricata-json-consumer/
 
-# adjust the permisions
-# sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.sh
-# sudo chmod a+x /opt/suricata-json-consumer/svc-suricata-json-consumer.sh
-
-# copy start-up files and add the Ubuntu service
+# create the system v service and run it at startup
 sudo cp svc-suricata-json-consumer.sh /etc/init.d/suricata-json-consumer
 sudo chmod a+x /etc/init.d/suricata-json-consumer
 sudo update-rc.d suricata-json-consumer defaults
 sudo update-rc.d suricata-json-consumer enable
 
-#sudo cp suricata-json-consumer.service /etc/systemd/system/
-#sudo systemctl daemon-reload
-#sudo systemctl enable suricata-json-consumer.service
-#sudo systemctl start suricata-json-consumer.service
+/etc/init.d/suricata-json-consumer start
