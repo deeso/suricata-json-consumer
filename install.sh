db006091d05122f01f12c50b907760830e4d3fe4
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-01-21 13:34:43 -0600

    need to be able to supply a 's3kr3t' config

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+MASTER_CONFIG=suricata-json-consumer.toml
 
+if [ -n "$1"  ]
+    then
+      MASTER_CONFIG=$1
+fi
 # it might be running?
 /etc/init.d/suricata-json-consumer stop
 # install the required python packages
@@ -8,7 +13,7 @@ pip3 install -I --force-reinstall -r requirements.txt
 # create the required directories and copy the supporting files
 sudo mkdir /opt/suricata-json-consumer
 sudo cp suricata-json-consumer.py /opt/suricata-json-consumer/
-sudo cp suricata-json-consumer.toml /opt/suricata-json-consumer/
+sudo cp $MASTER_CONFIG /opt/suricata-json-consumer/
 sudo cp suricata-json-consumer.sh /opt/suricata-json-consumer/
 sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.py
 sudo chmod a+x /opt/suricata-json-consumer/suricata-json-consumer.sh
